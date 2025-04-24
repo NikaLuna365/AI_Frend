117d6150ffe9df3630ebcd9b5002102174da9c1f
Nika_Lu <stevetemor@gmail.com>
2025-04-24 17:31:27 +0300

    Create models.py

---
+++ app/db/models.py
@@ -0,0 +1,4 @@
+# Импорт моделей для Alembic
+from app.core.users.models import User, Message
+from app.core.achievements.models import Achievement, AchievementRule
+from app.core.reminders.models import Reminder
