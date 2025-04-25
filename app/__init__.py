03a92d3dcef9c4a201d952fe64b915baca3b0038
Nika_Lu <stevetemor@gmail.com>
2025-04-25 13:07:52 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 # app/workers/__init__.py
 """
-Пакет для фоновых задач Celery.
-Ничего импортировать автоматически не нужно —
-Celery сам загрузит app.workers.tasks по флагу «-A».
+Пакет Celery-задач.
 """
 __all__: list[str] = ["tasks"]
