5aa785e847615f968dcbbd4bb71d5bb651197e27
Nika_Lu <stevetemor@gmail.com>
2025-04-25 14:15:58 +0300

    Update achievements.py

--- app/api/v1/achievements.py
+++ app/api/v1/achievements.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter
+# app/api/v1/achievements.py
 from typing import List
+
+from fastapi import APIRouter, HTTPException
+
 from app.core.achievements.service import AchievementsService
 from app.core.achievements.schemas import AchievementOut
 
-router = APIRouter()
+router = APIRouter(prefix="/v1/achievements", tags=["Achievements"])
 
-@router.get('/{user_id}', response_model=List[AchievementOut])
+
+@router.get("/{user_id}", response_model=List[AchievementOut])
 def get_achievements(user_id: str):
     svc = AchievementsService()
     return svc.list_user_achievements(user_id)
